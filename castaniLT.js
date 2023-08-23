94e1e9bbb96697e9156597e34809c2987cd8c4ca
arteyco <37159631+arteyco@users.noreply.github.com>
2023-08-23 14:21:34 -0500

    Add files via upload

--- castaniLT.js
+++ castaniLT.js
@@ -1,24 +1,24 @@
 function setup() {
-  createCanvas(900, 300);
-  noStroke();
-  fill(0);
-}
-function draw() {
-  background(204);
-  if (mouseX < 300) {
-    rect(0, 0, 300, 300);  // Left
+    createCanvas(displayWidth, displayHeight);
+    noStroke();
+    fill(0);
   }
-  else if (mouseX < 600) {
-    rect(300, 0, 300, 300); // Middle
-    
-	// Create a anchor object using createA() function
-	link = createA("https://www.geeksforgeeks.org/",
-					"GoGG", "_blank");
-	
-	// Position the anchor objects
-	link.position(430, 140);
+  function draw() {
+    background(204);
+    if (mouseX < displayWidth/3) {
+      rect(0, 0, displayWidth/3, displayHeight);  // Left
+    }
+    else if (mouseX < displayWidth/2) {
+      rect(displayWidth/3, 0, displayWidth/3, displayHeight); // Middle
+      
+      // Create a anchor object using createA() function
+      link = createA("https://www.geeksforgeeks.org/",
+                      "GoGG", "_blank");
+      
+      // Position the anchor objects
+      link.position(630, 240);
+    }
+    else {
+      rect(displayWidth, 0, displayWidth/3, displayHeight); // Right
+    }
   }
-  else {
-    rect(600, 0, 300, 400); // Right
-  }
-}
