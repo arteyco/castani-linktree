e6402f35e92867f7b702c3de9b3c22d0ad7bed50
arteyco <37159631+arteyco@users.noreply.github.com>
2023-08-23 19:04:49 -0500

    Add files via upload

--- castaniLT.js
+++ castaniLT.js
@@ -10,18 +10,24 @@ function setup() {
     background(204);
     if (mouseX < W/3) {
       rect(0, 0, W/3, H);  // Left
+            // Create a anchor object using createA() function
+            link = createA("https://teia.art/objkt/824511",
+            "GoGG-1", "_blank");
     }
     else if (mouseX < displayWidth/1.5) {
       rect(W/3, 0, W/3, H); // Middle
       
       // Create a anchor object using createA() function
-      link = createA("https://www.geeksforgeeks.org/",
-                      "GoGG", "_blank");
+      link = createA("https://teia.art/objkt/817364",
+                      "GoGG-2", "_blank");
       
       // Position the anchor objects
       link.position(650, 440);
     }
     else {
       rect(910, 0, W/3, H); // Right
+            // Create a anchor object using createA() function
+            link = createA("https://teia.art/objkt/801719",
+            "GoGG-3", "_blank");
     }
   }
